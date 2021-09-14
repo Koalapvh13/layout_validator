cbe83cad33c84ec40a3ca7ec849bcfb300845fb7
Matheus Dias Vieira <matheuspvh13@gmail.com>
2021-09-14 18:01:52 -0400

    Implementado o método que valida a quantidade de campos do layout

--- src/LayoutValidator.php
+++ src/LayoutValidator.php
@@ -4,6 +4,7 @@ class LayoutValidator
 {
     private $vidas = [];
     private const TAMANHO_CODIGO_CONTRATO = 4;
+    private const QUANTIDADE_CAMPOS = 6;
 
     public function __construct($vidas)
     {
@@ -14,4 +15,19 @@ class LayoutValidator
     {
         return strlen($codigoContrato) == self::TAMANHO_CODIGO_CONTRATO;
     }
-}
+
+    private function checkQtdCampos($index)
+    {
+        return count($this->vidas[$index]) == self::QUANTIDADE_CAMPOS;
+    }
+
+    public function validadar()
+    {
+        $qtd_vidas = count($this->vidas);
+        
+        for($i =0; $i < $qtd_vidas; $i++){
+            $qtd_campos = $this->checkQtdCampos($i);
+            echo $qtd_campos ? null: "Quantidade de campos diferente do esperado!";
+
+        }
+    }
