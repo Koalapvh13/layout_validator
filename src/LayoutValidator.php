e66250cf5bfd3d5443df53f5214a9c134aeaaf59
Matheus Dias Vieira <matheuspvh13@gmail.com>
2021-09-19 18:13:30 -0400

    Adicionado validação do sexo e cpf

--- src/LayoutValidator.php
+++ src/LayoutValidator.php
@@ -10,7 +10,7 @@ class LayoutValidator
     {
         $this->vidas = $vidas;
     }
-    
+
 
     private function checkQtdCampos($index)
     {
@@ -30,14 +30,14 @@ class LayoutValidator
     private function checkDependencia($index)
     {
         $dependencia = $this->vidas[$index];
-        $dep_valida = intval($dependencia) < 100 && intval($dependencia) >=0;
+        $dep_valida = intval($dependencia) < 100 && intval($dependencia) >= 0;
         return is_numeric($dependencia) ? $dep_valida : false;
     }
 
     private function checkNome($index)
     {
         $nome = $this->vidas[$index];
-        $conteudo = preg_replace('/[áàãâä]/ui', 'a', $nome); 
+        $conteudo = preg_replace('/[áàãâä]/ui', 'a', $nome);
         $conteudo = preg_replace('/[éèêë]/ui', 'e', $conteudo);
         $conteudo = preg_replace('/[íìîï]/ui', 'i', $conteudo);
         $conteudo = preg_replace('/[óòõôö]/ui', 'o', $conteudo);
@@ -48,22 +48,57 @@ class LayoutValidator
 
     private function checkData($index)
     {
-        $data = $this->vidas[$index] ?? '00112222';
-        $dia = substr($data,0,1);
-        $mes = substr($data,2,3);
-        $ano = substr($data,4,7);
+        $data = $this->vidas[$index];
+        $dia = substr($data, 0, 1);
+        $mes = substr($data, 2, 3);
+        $ano = substr($data, 4, 7);
         $dataValida = checkdate($mes, $dia, $ano);
         return strlen($data) > 0 ? $dataValida : false;
+    }
+
+    private function checkSexo($index)
+    {
+        $sexo = $this->vidas[$index];
+        return strlen($sexo) > 0;
+    }
+
+    private function checkCPF($index)
+    {
+
+        $cpf = $this->vidas[$index];
+        // Extrai somente os números
+        $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+
+        // Verifica se foi informado todos os digitos corretamente
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        // Faz o calculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public function validadar()
     {
         $qtd_vidas = count($this->vidas);
-        
-        for($i =0; $i < $qtd_vidas; $i++){
-            $qtd_campos = $this->checkQtdCampos($i);
-            echo $qtd_campos ? null: "Quantidade de campos diferente do esperado!";
 
+        for ($i = 0; $i < $qtd_vidas; $i++) {
+            $qtd_campos = $this->checkQtdCampos($i);
+            echo $qtd_campos ? null : "Quantidade de campos diferente do esperado!";
         }
     }
 }
